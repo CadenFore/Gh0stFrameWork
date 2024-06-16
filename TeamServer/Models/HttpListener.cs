5fdc55d5023531fb460a7c85ff76bad54f432d9e
Gh0st <Gh0st@DESKTOP-RK5VD92>
2024-06-16 15:49:09 -0700

    Add project files.

---
+++ TeamServer/Models/HttpListener.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace TeamServer.Models
+{
+    public class HttpListener : Listener
+    {
+        public override string Name { get;  }
+
+        public int BindPort { get; } 
+
+        public HttpListener(string name, int bindPort) 
+        {
+            Name = name;
+            BindPort = bindPort;
+        }
+
+        public override Task Start()
+        {
+            var hostBuilder = new HostBuilder()
+                 .ConfigureWebHostDefaults(host =>
+                 {
+                     host.UseUrls($"http://0.0.0.0:{BindPort}");
+                     host.Configure(ConfigureApp);
+                     host.ConfigureServices(ConfigureServices);
+                 });
+        }
+
+
+        //9:45 
+        private void ConfigureServices(IServiceCollection collection)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConfigureApp(IApplicationBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
